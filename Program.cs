1e62d6e3fbc88d662fd70dd9edc81a88e29e93b5
Frotrej <f.faszczowy@gmail.com>
2025-10-21 15:07:34 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Totalizator_Lotto_Api_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+			//https://developers.lotto.pl/api/open/v1/lotteries/info/game-jackpot
+			//ZFH07XmjGMFVOP37+Gf643PLK2p7JO1QDDcchRTXmIk=
+
+			var httpClient = new HttpClient();
+            
+            httpClient.DefaultRequestHeaders.Add("secret", "ZFH07XmjGMFVOP37+Gf643PLK2p7JO1QDDcchRTXmIk=");
+			httpClient.
+            
+            httpClient.GetStringAsync("https://developers.lotto.pl/api/open/v1/lotteries/info/game-jackpot?gameType=Lotto").Wait();
+            
+
+		}
+    }
+}
