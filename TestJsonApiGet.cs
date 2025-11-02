7e4e0c4794c3c3b7f69b0d4b99f1d260b430aab0
Frotrej <f.faszczowy@gmail.com>
2025-11-02 11:59:11 +0100

    implemented sample Get call to json api

---
+++ TestJsonApiGet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Totalizator_Lotto_Api_Client
+{
+	internal class TestJsonApiGet
+	{
+		public static async Task GetAsync()
+		{
+			HttpClient httpClient = new() { BaseAddress = new Uri("https://jsonplaceholder.typicode.com"), };
+
+			HttpResponseMessage response = await httpClient.GetAsync("todos");
+
+			response.EnsureSuccessStatusCode();
+
+			var jsonResponse = await response.Content.ReadAsStringAsync();
+
+			Console.WriteLine(jsonResponse);
+
+			Console.WriteLine(httpClient.BaseAddress.ToString());
+		}
+	}
+}
