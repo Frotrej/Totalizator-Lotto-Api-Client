552495f38d0d2619c28930f7ea466a1cadee2735
Frotrej <f.faszczowy@gmail.com>
2025-11-18 21:25:19 +0100

    Refined try catch block for secretKey

---
+++ PrepareHttpClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Totalizator_Lotto_Api_Client
+{
+	internal static class PrepareHttpClient
+	{
+
+		public static HttpClient GetHttpClient()
+		{
+			HttpClient myHttpClient = new();
+
+			//apply base url
+			var baseUrlAsUri = new Uri("https://developers.lotto.pl");
+			myHttpClient.BaseAddress = baseUrlAsUri;
+
+			//apply secret key from file
+			readSecretKeyFromFile(myHttpClient);
+
+			return myHttpClient;
+		}
+
+		private static void readSecretKeyFromFile(HttpClient myHttpClient)
+		{
+			try
+			{
+				string secretKey = File.ReadAllText(@"C:\Users\Kompjuter\Desktop\LottoApiKeyHere.txt");
+				myHttpClient.DefaultRequestHeaders.Add("secret", secretKey);
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("Błąd podczas odczytu secret key: Plik nie został znaleziony.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Błąd podczas odczytu secret key | Nieoczekiwany błąd: {ex}.");
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,29 @@
-﻿namespace Totalizator_Lotto_Api_Client
+﻿using System.Net.Http;
+
+namespace Totalizator_Lotto_Api_Client
 {
 	internal class Program : TestJsonApiGet
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			TestJsonApiGet.GetAsync().Wait();
+			string endpoint = "api/open/v1/lotteries/info/next-draw?gameType=Lotto";
+			string? jsonResponse = string.Empty;
+
+			HttpClient myHttpClient = PrepareHttpClient.GetHttpClient();
+
+			//zapytanie do API o najlbizsze losowanie Lotto
+			if ( myHttpClient.DefaultRequestHeaders.Any())
+			{
+				jsonResponse = await HttpRequestHelper.GetJsonAsync(myHttpClient, endpoint);
+
+				Console.WriteLine(jsonResponse);
+			}
 		}
 	}
 }
 
 
+// api/open/v1/lotteries/info/game-jackpot
 
 
 
