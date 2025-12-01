f21c528371299b3738c017890dfcc05296b02bd3
Frotrej <f.faszczowy@gmail.com>
2025-12-01 22:07:34 +0100

    appsetings.json

---
+++ Options/LottoOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Totalizator_Lotto_Api_Client.Options
+{
+	public record LottoOptions
+	{
+		public string ApiKey { get; set; }
+	}
+}
